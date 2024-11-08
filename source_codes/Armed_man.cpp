a03847843e502f4d86576925a3d843538d556675
Wildude <firaolder74@gmail.com>
2024-11-08 23:12:41 +0300

    Fixed armed man (Right side)

--- source_codes/Armed_man.cpp
+++ source_codes/Armed_man.cpp
@@ -18,6 +18,10 @@ int main(int argn, char** args)
     man.magnify(2);
     SDL_Event event;
     //wep.setmaincenter(Vflt2(-10, wep.image.getdst().h/2));
+    SDL_Surface* surf = SDL_CreateRGBSurface(0, 2, 2, 8, 0, 0, 0, 0);
+    SDL_SetSurfaceColorMod(surf, 0, 155, 200);
+    TEXTURE point(surf, win.getren());
+    point.queryF();
     while(event.type != SDL_QUIT){
         SDL_PollEvent(&event);
         if(SDL_GetKeyboardState(NULL)[SDL_SCANCODE_ESCAPE])break;
@@ -29,9 +33,10 @@ int main(int argn, char** args)
         if(event.button.button == 1)man.body[Upper_bod].Position = mousepos;
         bool reverted = (wep.Position.getx() > mousepos.getx());
         Vflt2 gunbarrel(
-            wep.Position.getx() + 20 * cos(torad(wep.angle - (reverted ? 0 : 0))),
-            wep.Position.gety() - 20 * sin(torad(wep.angle - (reverted ? 0 : 0)))
+            wep.Position.getx() + wep.Dimension.getx() / 1.3 * cos(torad(wep.angle)),
+            wep.Position.gety() - wep.Dimension.getx() / 1.3 * sin(torad(wep.angle))
         );
+        wep.Position = man.body[Lower_armL].Position + Vflt2(man.body[Lower_armL].Dimension.gety() / 4 * cos(torad(anglm)), -man.body[Lower_armL].Dimension.gety() / 4 * sin(torad(anglm)));
         double anglegun = angle_bn(man.body[Lower_armR].Position, gunbarrel);
         if(SDL_GetKeyboardState(NULL)[SDL_SCANCODE_LEFT]){man.animate2();}
         else if(SDL_GetKeyboardState(NULL)[SDL_SCANCODE_RIGHT]){man.animate();}
@@ -40,16 +45,19 @@ int main(int argn, char** args)
                 movement = false;
         }
         TXT txt("Pos", win.getren(), 15, 255, 0, 0, 255);
-        txt.board.set_dstpos(wep.Position.getx(), wep.Position.gety());
-        man.body[Upper_armL].angle = anglm + 90;
-        // man.body[Upper_armR].angle = anglm + 90;
-        man.body[Lower_armL].angle = man.body[Upper_armL].angle + 45;
+        txt.board.set_dstpos(gunbarrel.getx(), gunbarrel.gety());
+        point.set_dstpos(gunbarrel.getx(), gunbarrel.gety());
+        man.body[Head].angle = anglm;
+        man.body[Upper_armL].angle = anglm;
+        man.body[Upper_armR].angle = anglm + 45;
+        man.body[Lower_armL].angle = man.body[Upper_armL].angle + 90;
         man.body[Lower_armR].angle = anglegun + 90;
         wep.set_target(mousepos);
         //wep.Position = man.body[HandL].Position;
         wep.aim();
         man.draw();
         wep.draw();
+        point.drawC();
         txt.board.drawC();
         win.pst();
         win.clr();
