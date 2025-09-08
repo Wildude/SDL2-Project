1743227e3983a8f199e7a6eddeb10f903f22b20a
Aman Atalay <amanatalay00@gmail.com>
2025-09-08 21:03:07 +0300

    Complete SDL2 project build and Visual Studio setup
    - Build all 37 SDL2 demo executables with MinGW
    - Add Visual Studio solution and project files
    - Copy all required assets (fonts, images, DLLs) to Executables folder
    - Update log files with build and runtime information
    - Include Lua scripts, XML tilemaps, and multimedia assets
    - Ready-to-run configuration with all dependencies linked

---
+++ Executables/Files/XML/Interior-01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Interior-01" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="../Images/Stages/Tilesets/Interior-01.png" width="400" height="400"/>
+</tileset>
---
+++ Executables/Files/scripts/UI_test.lua
@@ -0,0 +1,42 @@
+-- Description: Test UI elements in Lua
+MainFont = {
+    path = "../Fonts/ROCK.ttf",
+    size = 50
+}
+MainColor = {
+    fcolor = {
+        r = 0,
+        g = 0,
+        b = 0,
+        a = 255
+    },
+    bcolor = {
+        r = 255,
+        g = 255,
+        b = 255,
+        a = 255
+    }
+}
+-- click
+ChangeFont = {
+    path = "../Fonts/ROCKB.ttf",
+    size = 50
+}
+-- hover
+ChangeColor = {
+    fcolor = {
+        r = 255,
+        g = 0,
+        b = 0,
+        a = 255
+    },
+    bcolor = {
+        r = 255,
+        g = 255,
+        b = 255,
+        a = 255
+    }
+}
+-- commands
+MenuItems = { "Play", "Options", "About", "Exit"}
+
