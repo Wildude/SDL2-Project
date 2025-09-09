31facef865224d75cc1950bd3322d06b52edf2d5
Wildude <firaolder74@gmail.com>
2025-09-09 21:04:17 +0300

    Removed Executables folder.

--- Executables/Files/XML/grassland_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="grassland_tiles" tilewidth="16" tileheight="16" tilecount="5376" columns="64">
- <image source="../Images/Stages/Tilesets/grassland_tiles.png" width="1024" height="1344"/>
-</tileset>
