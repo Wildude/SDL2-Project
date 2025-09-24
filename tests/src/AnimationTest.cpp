b698f3065fb1709c3e79717c78be9570253d8ab8
Wildude <firaolder74@gmail.com>
2025-09-24 20:47:12 +0300

    created animation test file.

---
+++ tests/src/AnimationTest.cpp
@@ -0,0 +1,11 @@
+#include <anim.hpp>
+#include <texture2D.hpp>
+#include <window.hpp>
+int main(int argn, char** args){
+    WINDOW win("Animation test1");
+    win.crtB();
+    win.pstcol(255, 255, 255, 255);
+    LinearSprite anim1;
+    Texture2D atlas;
+    return 0;    
+}
