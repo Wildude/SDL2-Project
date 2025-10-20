6b72469ac2401a4b856e3489ec120799368bd380
Wildude <firaolder74@gmail.com>
2025-10-20 21:36:25 +0300

    Fixed UI_test

--- tests/src/UI_test.cpp
+++ tests/src/UI_test.cpp
@@ -6,58 +6,47 @@ int main(int argn, char** args){
     WINDOW win("GUI Test");
     win.crtB();
     win.pstcol(255, 255, 255, 255);
-    std::cout << " creating play:\n";
+    //std::cout << " creating play:\n";
     Label play("play");
+    //std::cout << " created play:\n";
     play.setPos(100, 100);
-    SDL_Color rfg = {0, 0, 0, 255}, rbg = {255, 255, 255, 255};
-    std::cout << " Creating new Font:\n";
+    SDL_Color rfg = {0, 255, 0, 255}, rbg = {0, 0, 255, 255};
+    //std::cout << " Creating new Font:\n";
     FONT newFont("../Fonts/ROCKB.ttf", 70);
-    std::cout << " Creating rev Font:\n";
+    //std::cout << " Creating rev Font:\n";
     FONT revFont("../Fonts/nyala.ttf", 70);
-    std::cout << " setting play font:\n";
+    //std::cout << " setting play font:\n";
     play.setFont(revFont);
     play.setCol1(rfg);
     play.setCol2(rbg);
     SDL_Color nfg = {255, 0, 0, 255}, nbg = {0, 0, 255, 255};
     //
+    //std::cout << " creating commands:\n";
     UIColor changeColor(nbg, nfg);
     UIFont changeFont(newFont);
-    UIColor revertCol(rfg, rbg);
+    UIColor revertCol(rbg, rfg);
     UIFont revertFont(revFont);
-    revertCol.setref(play);
-    revertFont.setref(play);
     UIMultiCommand reverter;
     reverter.push(revertCol);
     reverter.push(revertFont);
-    reverter.setref(play);
     play.onFocus(&changeColor);
+    //std::cout << " setting click cmd:\n";
     play.onClick(&changeFont);
+    //std::cout << " setting revert cmd:\n";
     play.onRevert(&reverter);
-    /*
-    std::vector<Label> menu;
-    menu.push_back(Label("Play"));
-    menu.push_back(Label("Options"));
-    menu.push_back(Label("About"));
-    menu.push_back(Label("Exit"));
-    
-    UICommand* cmds [2]; // hover and revert
-    cmds[0] = &changeColor;
-    cmds[1] = &revert;
-    */
     InputManager input;
     SDL_Texture* texture = NULL;
     bool oldclick = false;
     while(!input.shouldQuit()){
         input.update();
         SDL_Point point;
-        bool click = false;
-        bool revert = true;
         SDL_GetMouseState(&point.x, &point.y);
         play.update(input);
-        play.render(win.getren());
+        play.render(win.getren(), 6);
         //Mcon.render(win.getren(), texture);
         win.pst();
         win.clr();
         SDL_Delay(33);
     }
+    return 0;
 }
