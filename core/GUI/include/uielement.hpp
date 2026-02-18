9c4eb8f19f9f439ba4d2b66b257997b0a19c3f79
Wildude <firaolder74@gmail.com>
2026-02-19 02:52:33 +0300

    defined important functions

--- core/GUI/include/uielement.hpp
+++ core/GUI/include/uielement.hpp
@@ -2,6 +2,11 @@
 #include <boxmodel.hpp>
 #include <inputManager.hpp>
 #include <font.hpp>
+#include <SDL2_gfx/SDL2_gfxPrimitives.h>
+inline std::ostream& operator<<(std::ostream& os, const SDL_Color& col){
+    os << "(" << (int)col.r << ", " << (int)col.g << ", " << (int)col.b << ", " << (int)col.a << ")";
+    return os;
+}
 class UIelement // : public GameObject
 {
     protected:
@@ -10,18 +15,38 @@ class UIelement // : public GameObject
     SimpleBox box;
     //
     public:
+    UIelement(): 
+    ishover(false), isfocus(false), isclick(false), 
+    isrevert(false), statechanged(false), iscurrent(false),
+    fg({0, 0, 0, 0}), bg({255, 255, 255, 255}), box() {}
     bool isclicked(InputManager& input);
     bool ishovered(InputManager& input);
     bool isfocused(InputManager& input);
     bool isreverted(InputManager& input);
+    // discrete testers
+    bool getclick() const;
+    bool gethover() const;
+    bool getfocus() const;
+    bool getrevert() const;
+    bool getcurrent() const;
+    bool getstate() const;
+    //
+    bool& getclick();
+    bool& gethover();
+    bool& getfocus();
+    bool& getrevert();
+    bool& getcurrent();
+    bool& getstate();
+    //
     virtual void setPos(int x, int y) = 0;
     virtual void setfg(const SDL_Color& col);
     virtual void setbg(const SDL_Color& col);
+    //
     virtual SDL_Color* getfg();
     virtual SDL_Color* getbg();
     // returns a pointer to the font (pure virtual, must be implemented by derived classes)
     virtual FONT* getFont() = 0;
     virtual SimpleBox* getBox();
     virtual void update(InputManager& input);
-    virtual void render(SDL_Renderer* rend, int drawtype = 2) = 0;
+    virtual void render(SDL_Renderer* rend, int drawtype = 2);
 };
